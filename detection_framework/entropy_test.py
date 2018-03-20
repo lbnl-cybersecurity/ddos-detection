cdfff648da3dec19ad717a2ce89732a8f60b35ed
rkgegan <rkgegan@ucdavis.edu>
2018-03-20 15:41:50 -0700

    Add files via upload

--- detection_framework/entropy_test.py
+++ detection_framework/entropy_test.py
@@ -31,8 +31,15 @@ class Entropy:
 		self.target_flows = 0
 		self.target = "204.38.0.0/21"
 		self.flow_count = 0
+		self.no_dump = False
 		self.log_entry = ""
-
+		self.total_pkts = 0
+		self.total_flows = 0
+		
+		# Settings
+		self.feature = "sp"
+		self.data_type = "netflow"
+		self.ent_type = "avg_size"
 
 	# Record the log file
 	def write2file(self):
@@ -57,26 +64,41 @@ class Entropy:
     		row = [count]
     		#print("calculating entropy for file %d",count)
     		### calculate current entropy
+		ent_result = ""
 		self.log_entry = ""
+		ent_result += str(self.total_pkts)
+		ent_result += ","
+		ent_result += str(self.total_flows)
+		ent_result += ","
     		for field in self.options:
                 	#print(field)
                 	hx = 0
-                	total = sum([self.curr_s_dict[field][k] for k in self.curr_s_dict[field]])
-                	#print(total)
+			total = 0
+			total2 = 0 
+			if field in self.curr_s_dict:
+                		total = sum([self.curr_s_dict[field][k] for k in self.curr_s_dict[field]])
+                		for element in self.curr_s_dict[field]:
+					total2 += self.curr_s_dict[field][element]	
+				#print total2
+			#print "total1 %d" % (total)
                 	for element in self.curr_s_dict[field]:
                         	#  update destinationCounter to get top destinations
                         	nx = self.curr_s_dict[field][element]
-
+				#print "nx %d" % (nx)
 				# Find the destination with the largest volume
 				# Update this to include more than just the top target
 				# Important to check for other possible targets
-                        	if field in "da":
+                        	if field in "da": 
                                 	if nx > topDest:
-                                        	topDest = nx
-                                       		topIP = element
+                                		topDest = nx
+                                		topIP = element
 
                         	px = nx/float(total)
-                        	hx += -1*px*log(px, 2)
+				if px != 0:
+                        		hx += -1*px*log(px, 2)
+				else:
+					print field
+					print "no"
                 	N0 = log(len(self.curr_s_dict[field]), 2)
                 	#print(N0)
                 
@@ -84,19 +106,17 @@ class Entropy:
                         	hx_norm = hx/float(N0)
                         	# check thresholds here
 
-                        	#for element in self.curr_s_dict[field]:
-                                	#print(element)
-				if field in "da":
-                        		print(hx_norm)
-                        	if field in "da" and hx_norm < 0.95: #0.25:
-					self.log_entry = "%s, %s, low entropy: %s, potential DDoS attack" % (topIP, topDest, hx_norm)
-                                	#print(topIP,topDest)
-                                	#print("Potential DDoS attack found")
-                                	#ff = open(outfile, 'ab')
-                                	#print("Target is: ", topIP, " Volume", topDest)
-                                	#print(" Entropy: ", hx)
-                                	#print(" Potential DDoS attack found\n")
-    
+                        	ent_result += str(hx_norm)
+				if field in "dp":
+					print ent_result
+				else:
+					ent_result += ","
+					#if hx_norm == 1.0:
+						#print self.curr_s_dict['da']
+                        	if field in self.feature:
+					if hx_norm <= self.threshold:
+						self.log_entry = "%s, %s, low entropy: %s, potential DDoS attack" % (topIP, topDest, hx_norm)
+                                	
 	def dump2(self, count):
     		row = [count]
     		print("Recording blank file", count)
@@ -122,22 +142,32 @@ class Entropy:
 	# Update the counts for the entropy calculation
 	# Call this function for each new flow
 	def entropy(self, row):
-    		#if row[1] in target: # if the target is the destination, update the total count 
-    		#	target_count += row[5]
-    		#    target_flows += 1
-
+    		
     		for i, field in enumerate(self.options):
-            		#print "counting"
             		#count(field, row[i], row[5])
             		if not field in self.curr_s_dict:
                 		self.curr_s_dict[field] = {}
 
             		if not row[i] in self.curr_s_dict[field]:
-               			self.curr_s_dict[field][row[i]] = row[5]
+				if self.ent_type == "avg_size":
+               				self.curr_s_dict[field][row[i]] = row[5]/row[6]
+				else if self.ent_type == "pkt_size":
+					self.curr_s_dict[field][row[i]] = row[5]
+				else if self.ent_type == "pkt_count":
+					self.curr_s_dict[field][row[i]] = row[6]
+				#print self.curr_s_dict[field][row[i]]
             		else:
-                		self.curr_s_dict[field][row[i]] += row[5]
+				if self.ent_type == "avg_size":
+                			self.curr_s_dict[field][row[i]] += row[5]/row[6]
+				else if self.ent_type == "pkt_size":
+					self.curr_s_dict[field][row[i]] += row[5]
+				else if self.ent_type == "pkt_count":
+					self.curr_s_dict[field][row[i]] += row[6]
 
-	# Perform the test on an nfdump file
+			if self.curr_s_dict[field][row[i]] == 0:
+				self.curr_s_dict[field][row[i]] += 1
+
+	# Perform the entropy test on an nfdump file
 	def detect_entropy(self, nfdump):
     		count = 0
     		item_count = 0
@@ -160,3 +190,102 @@ class Entropy:
     		# Calculate entropy score and check for potential DDoS targets
     		self.dump(0)
     		self.curr_s_dict = {}
+			
+	# Perform the test on a tstat file (use for testing dtn data)
+	def detect_entropy_ts_alt(self, tstat):
+    		count = 0
+    		item_count = 0
+    		# Read the tstat file, flow-by-flow
+    		with open(tstat, 'rb') as csvfile:
+        		nfreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        		for row in nfreader:
+				#print row
+                		if count >= 1:
+                        		#print ', '.join(row)
+                        		ent_data = []
+				
+                        		ent_data.append(row[4]) # src ip
+                        		ent_data.append(row[5]) # dst ip
+                        		ent_data.append(row[6]) # src port
+                        		ent_data.append(row[7]) # dst port
+                        		ent_data.append(row[8]) # packet count
+                        		ent_data.append(count) # count
+                        		#print(ent_data)
+                        		self.entropy(ent_data)
+                		count += 1
+    		# Calculate entropy score and check for potential DDoS targets
+		if 'sa' in self.curr_s_dict:
+			#print "ent count"
+			#print self.curr_s_dict['sa']
+    			self.dump(0)
+    		self.curr_s_dict = {}		
+			
+		
+
+        # Perform the entropy test on tstat data file
+        def detect_entropy_ts(self, tstat):
+
+    		count = 0
+    		item_count = 0
+    		# Read the tstat file, flow-by-flow
+    		with open(tstat, 'rb') as csvfile:
+        		nfreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        		for row in nfreader:
+				row2 = row[0].split()
+                		if count >= 1:
+                        		ent_data = []
+                        		ent_data.append(row2[0]) # src ip
+                        		ent_data.append(row2[1]) # dst ip
+                        		ent_data.append(row2[3]) # src port
+                        		ent_data.append(row2[4]) # dst port
+                        		ent_data.append(row2[5]) # packet count
+					#ent_data.append(row2[6])
+					#pkts
+                        		ent_data.append(int(row2[5])) # count (next try entropy using packet size?)
+					ent_data.append(int(row2[6])) # 5 = octets/size, 6 = packet count
+					if row2[1] == "35.7.72.0" or row2[0] == "35.7.72.0":
+					
+						self.entropy(ent_data)
+						self.total_pkts += int(row2[6])
+						self.total_flows += 1
+                		count += 1
+		#print "done"
+		self.dump(0)
+                self.curr_s_dict = {}
+    			
+        def detect_entropy_dtn(self, tstat):
+
+                count = 0
+                item_count = 0
+                # Read the tstat file, flow-by-flow
+                with open(tstat, 'rb') as csvfile:
+                        nfreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+                        for row in nfreader:
+                                row2 = row[0].split(":,")
+                                src = row[1].split(":")
+				dst = row[2].split(":")
+				
+                                if count >= 0:
+                                        #print ', '.join(row)
+                                        ent_data = []
+                   
+					#src[0] = src[0]+src[1]+dst[0]+dst[1]
+					#print src[0]
+
+                                        ent_data.append(src[0]) # src ip
+                                        ent_data.append(src[1]) # dst ip
+                                        ent_data.append(dst[0]) # src port
+                                        ent_data.append(dst[1]) # dst port
+                                        #ent_data.append(row[5]) # packet count
+					ent_data.append(row[5])
+                                        ent_data.append(int(row[3])+int(row[5])) # count
+					ent_data.append(int(row[4])+int(row[6]))
+                                        #print(ent_data)
+					if ent_data[5] != 0 and ent_data[6] != 0:
+                                        	self.entropy(ent_data)
+					self.total_pkts  += int(row[4]) + int(row[6])
+					self.total_flows += 1
+                                count += 1
+                #print "done dtn"
+		self.dump(0)
+                self.curr_s_dict = {}
---
+++ detection_framework/req_rsp.py
@@ -0,0 +1,153 @@
+# Entropy and volume based DDoS detection methods
+# Use these methods on flow data in detection_framework.py
+# Performs the test and creates the log file
+
+import socket
+import struct
+import os.path
+import time
+import sys
+import csv
+import getopt
+import collections
+import shutil
+import math
+import logging
+from subprocess import call
+from math import log
+
+from socket import inet_ntoa
+
+# Test for DDoS attacks by looking at  
+# abnormally high requests and responses
+class RequestResponse:
+	def __init__(self):
+        	self.curr_s_dict = {}
+		self.curr_tr_epoch = 0
+		self.options = ['sa', 'da', 'sp', 'dp']
+		self.outfile = 'ddosLog.txt'
+		self.result = []
+		self.grid = 500
+		self.target_count = 0
+		self.target_flows = 0
+		self.target = "204.38.0.0/21"
+		self.flow_count = 0
+		self.no_dump = False
+		self.log_entry = ""
+		self.results1 = []
+		self.results2 = []
+		self.results3 = []
+		self.results4 = []
+		self.results5 = []
+		self.results6 = []
+		self.results7 = []
+		self.results8 = []
+		self.results9 = []
+		self.max_pkts = 0 # highest packet count per 5 minutes
+		self.max_flow = "" # flow with highest pkts
+		self.max_pkts_req = 0
+		self.max_flow_req = ""
+
+		# Settings
+		self.protocol = "dns"
+		self.pkt_thresh = 10000
+		self.size_thresh = 900
+		  			
+        def detect_reflection(self, tstat):
+
+		# dictionary - key is request srcIP 
+		# 	       value is a list of flow data arrays
+		aggregate_reqs = {}
+		aggregate_rsps = {}
+
+		target = "207.75.112.0" #radb
+		#target = "204.38.0.0" #dns_ampl
+                count = 0
+                item_count = 0
+                # Read the tstat file, flow-by-flow
+                with open(tstat, 'rb') as csvfile:
+                        nfreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+                        for row in nfreader:
+                                row2 = row[0].split()
+                                #print row2
+                                if count >= 0:
+					# 53 is for dns protocol
+					# 19 is for chargen
+
+					if row2[4] == "19":# and row2[2] == "17":									
+						aggregate_reqs.setdefault(row2[0], []).append(ent_data)
+					if row2[3] == "19":# and row2[2] == "17":
+						aggregate_rsps.setdefault(row2[1],[]).append(ent_data)	
+					#print row2[0]
+					#if len(aggregate_reqs[row2[0]]) > 1:
+					#print aggregate_reqs							
+                        	count += 1
+
+			#  Check request total
+			for key in aggregate_reqs:
+				agg_pkts = 0
+				agg_size = 0
+				for flow_list in aggregate_reqs[key]:
+					if flow_list[5].isdigit() and flow_list[4].isdigit():
+						agg_pkts += int(flow_list[5])
+						agg_size += int(flow_list[4])
+				if agg_pkts > 1:
+					self.results1.append(str(agg_pkts))
+					self.results2.append(str(agg_size)) 
+			
+				if self.max_pkts_req < agg_pkts:
+                                        self.max_pkts_req = agg_pkts
+                                        self.max_flow_req = key
+	
+				elif agg_pkts > pkt_thresh:
+					# check the record more closely
+					# calculate standard deviation of request size
+					std_dev = 0
+					mean = agg_size/len(aggregate_reqs[key])
+					square_sum = 0
+					square = 0
+					for flow_list in aggregate_reqs[key]:
+						if flow_list[5].isdigit() and flow_list[4].isdigit():
+							square_sum += abs(int(flow_list[4]) - mean)*abs(int(flow_list[4]) - mean)
+				
+					square = square_sum/len(aggregate_reqs[key])
+					std_dev = math.sqrt(square)
+					
+				
+					if len(aggregate_reqs[key]) > agg_pkts/2 and square != 0:
+						if std_dev < 1:
+							print ""
+					else:
+						for flow_list in aggregate_reqs[key]:
+							if flow_list[2] not in "53":
+								# possible attack, raise alert
+								break
+		# Part 2 of detection test - Check responses
+		for key in aggregate_rsps:
+				agg_pkts = 0
+                                agg_size = 0
+                                for flow_list in aggregate_rsps[key]:
+                                        if flow_list[5].isdigit() and flow_list[4].isdigit():
+                                                agg_pkts += int(flow_list[5])
+                                                agg_size += int(flow_list[4])
+
+				self.results3.append(str(agg_pkts))
+				self.results4.append(str(agg_size))
+				
+				if self.max_pkts < agg_pkts:
+					self.max_pkts = agg_pkts 
+					self.max_flow = key
+
+				if target in key:
+                                        self.results6.append("attack")
+                                else:
+                                        self.results6.append("not attack")
+				self.results9.append(key)
+                                if agg_pkts > self.pkt_thresh:
+                                        # possible attack, raise alert
+					self.log_entry = "Potential Attack"
+
+                                elif agg_pkts > self.pkt_thresh/10:
+					if agg_size/len(aggregate_rsps[key]) > size_thresh:
+					# possible attack, raise alert
+					self.log_entry = "Potential attack"					
