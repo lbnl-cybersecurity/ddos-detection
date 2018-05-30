8a18c974adda8f4321110de32622fe5b975920d3
rkgegan <rkgegan@ucdavis.edu>
2018-05-29 20:37:04 -0700

    Add files via upload

--- detection_framework/globals.py
+++ detection_framework/globals.py
@@ -1,10 +1,1 @@
-thread_queues = [] # List of thread queues
-class ClientThread(threading.Thread):
-    def __init__(self):
-        myqueue = Queue.Queue() #Client queue
-        clientqueues.append(myqueue)
-        ...
-def MessageAllClients(message):
-    global clientqueues
-    for queue in clientqueues:
-        queue.put(message)
+test_vars = {}
--- detection_framework/response_test.py
+++ detection_framework/response_test.py
@@ -13,6 +13,7 @@ import collections
 import shutil
 import math
 import logging
+import globals
 from subprocess import call
 from math import log
 
@@ -86,9 +87,9 @@ class RequestResponse:
 
 
 					if row[6] == self.protocol_port:# and row2[2] == "17":									
-						aggregate_reqs.setdefault(row2[0], []).append(ent_data)
-					if row[5] == protocol_port:# and row2[2] == "17":
-						aggregate_rsps.setdefault(row2[1],[]).append(ent_data)	
+						aggregate_reqs.setdefault(row[3], []).append(ent_data)
+					if row[5] == self.protocol_port:# and row2[2] == "17":
+						aggregate_rsps.setdefault(row[4],[]).append(ent_data)	
 						
                         	count += 1
 
@@ -124,20 +125,12 @@ class RequestResponse:
 				#print "working DNS"
                                 if agg_pkts > self.pkt_thresh:
                                         # possible attack,raise alert
-					self.log_entry = "High DNS responses sent to %s, %d response packets" % (key, agg_pkts)
-				#else:
-					#self.log_entry = "DNS is normal"			#print self.log_entry
-
-
-                                #elif agg_pkts > self.pkt_thresh/10:
-				#	if agg_size/len(aggregate_rsps[key]) > size_thresh:
-					# possible attack, raise alert
-					#self.log_entry = "Potential attack"	
-
+					self.log_entry = "High responses sent to %s, %d response packets with port %s" % (key, agg_pkts,self.protocol_port)
+				
 	# Run the dns request test
 	def run_test(self, nfdump):
 		if "response_thresh" in globals.test_vars:
-			self.pkt_thresh = globals.test_vars["response_thresh"]
+			self.pkt_thresh = int(globals.test_vars["response_thresh"])
 		if "response_port" in globals.test_vars:
 			self.protocol_port = globals.test_vars["response_port"]
 		self.detect_reflection(nfdump)				
