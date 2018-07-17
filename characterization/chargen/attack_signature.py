82c7942e67c11c1f608e207b63d9c6208745851b
rkgegan <rkgegan@ucdavis.edu>
2018-07-17 14:20:46 -0700

    Update attack_signature.py

--- characterization/chargen/attack_signature.py
+++ characterization/chargen/attack_signature.py
@@ -1,5 +1,5 @@
 # Anonymization: 11 less significant bits of IPs are set to zero.
-target_ip = "35.7.72.0" #/21
+target_ip = "255.255.255.0" #/21
 target_sport = 19       # CHARGEN Port
 target_prot = 17        #UDP protocol
 attack_type = 'chargen'
