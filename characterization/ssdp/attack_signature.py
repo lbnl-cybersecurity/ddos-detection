6ee909f2a598ef6abdf1a56413027959f1fd65ee
rkgegan <rkgegan@ucdavis.edu>
2018-07-17 14:22:08 -0700

    Update attack_signature.py

--- characterization/ssdp/attack_signature.py
+++ characterization/ssdp/attack_signature.py
@@ -1,5 +1,5 @@
 # Anonymization: 11 less significant bits of IPs are set to zero. 
-target_ip = "198.111.64.0" #/21 
+target_ip = "255.255.255.0" #/21 
 target_sport = 1900  # SSDP Port 
 target_prot = 17    # UDP protocol
 attack_type = 'ssdp'
